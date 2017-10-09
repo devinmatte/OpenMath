7d2de0a058b726878535826acad0a5340a38b59f
Devin Matte <devinmatte@gmail.com>
2017-10-08 23:59:43 -0400

    Initial Upload

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {CounterButton, GithubButton} from 'components';
+import config from '../../config';
+import Helmet from 'react-helmet';
+
+export default class Home extends Component {
+  render() {
+    const styles = require('./Home.scss');
+    // require the logo image both from client and server
+    const logoImage = require('./logo.png');
+    return (
+      <div className={styles.home}>
+        <Helmet title="Home"/>
+        <div className={styles.masthead}>
+          <div className="container">
+            <div className={styles.logo}>
+              <p>
+                <img src={logoImage}/>
+              </p>
+            </div>
+            <h1>{config.app.title}</h1>
+
+            <h2>{config.app.description}</h2>
+
+            <p>
+              <a className={styles.github} href="https://github.com/devinmatte/OpenMath"
+                 target="_blank">
+                <i className="fa fa-github"/> View on Github
+              </a>
+            </p>
+            <GithubButton user="devinmatte"
+                          repo="OpenMath"
+                          type="star"
+                          width={160}
+                          height={30}
+                          count large/>
+            <GithubButton user="devinmatte"
+                          repo="OpenMath"
+                          type="fork"
+                          width={160}
+                          height={30}
+                          count large/>
+
+            <p className={styles.humility}>
+              Created and maintained by <a href="https://twitter.com/devinmatte" target="_blank">@devinmatte</a>.
+            </p>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className={styles.counterContainer}>
+            <CounterButton multireducerKey="counter1"/>
+            <CounterButton multireducerKey="counter2"/>
+            <CounterButton multireducerKey="counter3"/>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
